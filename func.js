f94c277482c18beb85e1ca60dfc1948173315f12
ice-forever <74451255+ice-forever@users.noreply.github.com>
2022-09-18 19:18:09 +0800

    20220918-1

---
+++ func.js
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+function showMoeCounter() {
+    x = document.getElementById("content"); // 找到元素
+    x.innerHTML = "<iframe align=\"center\" width=\"100%\" height=\"170\" src=\"./Moe-counter.html\"  frameborder=\"no\" border=\"0\" marginwidth=\"0\"marginheight=\"0\" scrolling=\"no\"> </iframe>"; // 改变内容
+}
