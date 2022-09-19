87b35a89b62ae60186bbacbe1423c925a45b1dfa
ice-forever <74451255+ice-forever@users.noreply.github.com>
2022-09-19 21:05:29 +0800

    主界面制作完成

--- func.js
+++ func.js
@@ -3,9 +3,9 @@
  */
 function showMoeCounter() {
     x = document.getElementById("page"); // 找到元素
-    x.innerHTML = "<iframe align=\"center\" width=\"100%\" height=\"100%\" src=\"./Moe-counter.html\"  frameborder=\"no\" border=\"0\" marginwidth=\"0\"marginheight=\"0\" scrolling=\"no\"> </iframe>"; // 改变内容
+    x.innerHTML = "<iframe align=\"center\" width=\"100%\" height=\"100%\" src=\"./Moe-counter.html\"  frameborder=\"no\" border=\"0\" marginwidth=\"0\"marginheight=\"0\" scrolling=\"yes\"> </iframe>"; // 改变内容
 }
 function showMain() {
     x = document.getElementById("page"); // 找到元素
-    x.innerHTML = "<iframe src=\"./Main.html\" align=\"center\" width=\"100%\" height=\"100%\"  frameborder=\"no\" border=\"0\" marginwidth=\"0\"marginheight=\"0\" scrolling=\"no\"> </iframe>"; // 改变内容
+    x.innerHTML = "<iframe src=\"./Main.html\" align=\"center\" width=\"100%\" height=\"100%\"  frameborder=\"no\" border=\"0\" marginwidth=\"0\"marginheight=\"0\" scrolling=\"yes\"> </iframe>"; // 改变内容
 }
