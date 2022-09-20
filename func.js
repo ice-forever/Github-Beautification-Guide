8814ec387668f2e47d8ea25f70cde3a8e451de67
ice-forever <74451255+ice-forever@users.noreply.github.com>
2022-09-20 16:22:03 +0800

    添加时间显示功能

--- func.js
+++ func.js
@@ -9,3 +9,18 @@ function showMain() {
     x = document.getElementById("page"); // 找到元素
     x.innerHTML = "<iframe src=\"./Main.html\" align=\"center\" width=\"100%\" height=\"100%\"  frameborder=\"no\" border=\"0\" marginwidth=\"0\"marginheight=\"0\" scrolling=\"yes\"> </iframe>"; // 改变内容
 }
+function showTime(clock) {
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth();
+    var day = now.getDate();
+    var hour = now.getHours();
+    var minu = now.getMinutes();
+    var second = now.getSeconds();
+    month = month + 1;
+    var arr_work = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
+    var week = arr_work[now.getDay()];
+    var time = year + "年" + month + "月" + day + "日 " + week + " " + hour + ":" + minu + ":" + second;
+    clock.innerHTML = "当前时间: " + time;
+}
+
